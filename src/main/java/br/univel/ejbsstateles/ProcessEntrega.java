b134485799c5fa3f39185ffaa79d35d23090e113
Fabricio <Fabricio@DESKTOP-0SVRQNJ>
2016-09-13 20:31:12 -0300

    #4

---
+++ src/main/java/br/univel/ejbsstateles/ProcessEntrega.java
@@ -0,0 +1,13 @@
+package br.univel.ejbsstateles;
+import javax.ejb.Stateless;
+
+import br.univel.model.Entrega;
+
+@Stateless
+public class ProcessEntrega {
+
+	public void processarEntrega (Entrega entrega) {
+
+    }
+
+}
---
+++ src/main/java/br/univel/ejbsstateles/ProcessVenda.java
@@ -0,0 +1,13 @@
+package br.univel.ejbsstateles;
+
+import javax.ejb.Stateless;
+import br.univel.model.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+	 public void processarVenda(Venda venda) {
+
+     }
+
+}
