163018369478c457ce4fa7888704690db9276ef8
Fabricio <Fabricio@DESKTOP-0SVRQNJ>
2016-09-20 19:51:48 -0300

    #7

--- src/main/java/br/univel/mdb/MdbLogistica.java
+++ src/main/java/br/univel/mdb/MdbLogistica.java
@@ -1,5 +1,46 @@
 package br.univel.mdb;
 
-public class MdbLogistica {
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(name = "MdbLogistica", activationConfig = {
+	    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/QueuePedido"),
+	    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class MdbLogistica implements MessageListener{
+
+	@Override
+	public void onMessage(Message rcvMessage) {
+		TextMessage msg = null;
+        try {
+            if (rcvMessage instanceof TextMessage) {
+                msg = (TextMessage) rcvMessage;
+                System.out.println("Received Message from "+this.getClass().getSimpleName()+" " + msg.getText());
+                new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(30000);
+							System.out.println("Message Dispatched!!");
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+				});
+            } else {
+                System.out.println("Message of wrong type: " + rcvMessage.getClass().getName());
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+
+	}
 
 }
